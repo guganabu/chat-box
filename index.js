e6b2eb4a939e5df097e1c8a8a863851cae8a0e5e
Gugan <guganssp@gmail.com>
2021-08-19 21:55:31 +0530

    Added cors option

--- index.js
+++ index.js
@@ -1,9 +1,45 @@
 const app = require("express")();
 // const httpServer = require("http").createServer(app);
-const options = { cors: {
-    origin: '*',
-  } };
 
+var cors = require('cors')
+const options = {
+    origins: ['https://guganabu.github.io'],
+
+    handlePreflightRequest: (req, res) => {
+      res.writeHead(200, {
+        "Access-Control-Allow-Origin": "'https://guganabu.github.io'",
+        "Access-Control-Allow-Methods": "GET,POST",
+      });
+      res.end();
+    }
+  };
+
+//   var corsOptions = {
+//     origin: 'https://guganabu.github.io',
+//     optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+//   }
+
+// app.use(cors(corsOptions))
+
+app.get('/', (req, res) => {
+  res.send('Web chat server running...')
+})
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+
+ // Add this
+ if (req.method === 'OPTIONS') {
+
+      res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, OPTIONS');
+      res.header('Access-Control-Max-Age', 120);
+      return res.status(200).json({});
+  }
+
+  next();
+
+});
  const server = app.listen(3000)
 
  const io = require("socket.io")(server, options)
